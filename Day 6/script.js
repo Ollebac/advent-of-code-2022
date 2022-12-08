54db747147378bc1ebd6c9d3be86ce8520f5f147
Ian <iancabello16@gmail.com>
2022-12-08 15:47:30 -0500

    Day 6

---
+++ Day 6/script.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const readline = require('readline');
+
+let count = 0;
+let found = false;
+let codedText = [], currentPacket = [], tempSet = [];
+
+//Part 1: Set {if count} to 4 and tempset.size} to 4
+//Part 2: Set {if count} to 14 and tempset.size} to 14
+
+function findMarker(code) {
+    codedText = code.split('');
+    while (count < codedText.length) {
+        if (count < 14) {
+            currentPacket.push(codedText[count]);
+            count += 1;
+        } else {
+            tempSet = new Set(currentPacket);
+            if (tempSet.size === 14) {
+                console.log(`found dis bitch at ${count}`)
+                console.log(tempSet);
+                found = true;
+                break;
+            }
+            currentPacket.push(codedText[count]);
+            currentPacket.shift();
+            count += 1;
+        }
+    }
+}
+
+async function processLineByLine() {
+    const myFile = fs.createReadStream('day6_input.txt');
+  
+    const eachLine = readline.createInterface({
+      input: myFile,
+      crlfDelay: Infinity
+    });
+
+    for await (const line of eachLine) {
+        findMarker(line);
+    }
+
+    console.log(`Characters process until first marker: ${count}`)
+}
+
+processLineByLine();
